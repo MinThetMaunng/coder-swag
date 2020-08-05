79ff20bd0639c142135d50b3e7ff6ce710e55c15
MinThetMaung <minthetmaung96@gmail.com>
2020-08-05 12:51:26 +0630

    connect IBoutlet in collection view cell

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Min Thet Maung on 05/08/2020.
+//  Copyright © 2020 Myanmy. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
