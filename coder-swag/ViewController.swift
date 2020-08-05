f60a20e060383ba636012b96f0688d593fa2c36e
MinThetMaung <minthetmaung96@gmail.com>
2020-08-05 10:54:27 +0630

    Project setup

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Min Thet Maung on 05/08/2020.
+//  Copyright © 2020 Myanmy. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
